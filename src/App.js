745786f9afded72d0e34c6555948a527c31f5776
bertrandda <bertrandda@laposte.net>
2018-11-13 17:49:00 +0100

    Finish client first version

--- src/App.js
+++ src/App.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import NestorTerminal from './lib/NestorTerminal';
 
+import Icon from '@mdi/react';
+import { mdiEmail, mdiFileDocument, mdiGithubCircle, mdiLinkedinBox } from '@mdi/js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NestorTerminal/>
+        <header>
+          <h1>Bienvenue</h1>
+          <div className="social">
+            <a href="mailto:bertrand.daure@laposte.net">
+              <Icon className="social-icon" path={mdiEmail}
+                size={1.5}
+                color="white" />
+            </a>
+            <a href="https://bertranddaure.fr">
+              <Icon className="social-icon" path={mdiFileDocument}
+                size={1.5}
+                color="white" />
+            </a>
+            <a href="https://github.com/bertrandda">
+              <Icon className="social-icon" path={mdiGithubCircle}
+                size={1.5}
+                color="white" />
+            </a>
+            <a href="https://www.linkedin.com/in/bertrand-d-aure-0386a0138">
+              <Icon className="social-icon" path={mdiLinkedinBox}
+                size={1.5}
+                color="white" />
+            </a>
+          </div>
+        </header>
+
+        <div className="nestor-terminal" >
+          <NestorTerminal />
+        </div>
+
+        <footer>
+          <div>© 2018 - Bertrand d'Aure</div>
+        </footer>
       </div>
     );
   }
