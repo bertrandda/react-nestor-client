5a182e3deea9912489229065b9355aa103e3bd73
bertrandda <bertrandda@laposte.net>
2018-11-15 17:33:38 +0100

    Open links in new tabs

--- src/App.js
+++ src/App.js
@@ -17,17 +17,17 @@ class App extends Component {
                 size={1.5}
                 color="white" />
             </a>
-            <a href="https://cv.bertranddaure.fr">
+            <a href="https://cv.bertranddaure.fr" target="_blank" rel="noopener noreferrer">
               <Icon className="social-icon" path={mdiFileDocument}
                 size={1.5}
                 color="white" />
             </a>
-            <a href="https://github.com/bertrandda">
+            <a href="https://github.com/bertrandda" target="_blank" rel="noopener noreferrer">
               <Icon className="social-icon" path={mdiGithubCircle}
                 size={1.5}
                 color="white" />
             </a>
-            <a href="https://www.linkedin.com/in/bertrand-d-aure-0386a0138">
+            <a href="https://www.linkedin.com/in/bertrand-d-aure-0386a0138" target="_blank" rel="noopener noreferrer">
               <Icon className="social-icon" path={mdiLinkedinBox}
                 size={1.5}
                 color="white" />
